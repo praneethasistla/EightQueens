a3fe39f61e7f5acf1b43d0c3b78130105ee94895
Praneetha Sistla <praneetha.sistla@gmail.com>
2015-06-11 17:44:51 -0400

    Eight Queens added

--- src/EightQueens.java
+++ src/EightQueens.java
@@ -8,9 +8,8 @@ public class EightQueens {
 		Queen q4 = new Queen();
 		Queen q5 = new Queen();
 		Queen q6 = new Queen();
-/*		Queen q7 = new Queen();
+		Queen q7 = new Queen();
 		Queen q8 = new Queen();
-	*/	
 		q1.setP(0, 0);
 		for (int i=0; i<8; i++){
 			//place the queen in a position
@@ -19,6 +18,8 @@ public class EightQueens {
 				if (q3.getState())
 					if(q4.getState())
 						if(q5.getState())
+							if(q6.getState())
+								if(q7.getState())
 							break;
 			
 			for (int j=0; j<8; j++){
@@ -30,6 +31,10 @@ public class EightQueens {
 					q4.setP(i, j);
 				if (!q5.getState())
 					q5.setP(i, j);
+				if (!q6.getState())
+					q6.setP(i, j);
+				if (!q7.getState())
+					q7.setP(i, j);
 				
 				if(!q2.isConflict(q1)){
 					if (!q2.getState()){
@@ -53,7 +58,6 @@ public class EightQueens {
 									i = 5;
 									j = 0;
 								}
-								q6.setP(i, j);
 								if(!q6.isConflict(q1) && !q6.isConflict(q2) && !q6.isConflict(q3) && !q6.isConflict(q4) &&
 										!q6.isConflict(q5)){
 									if (!q6.getState()){
@@ -61,8 +65,21 @@ public class EightQueens {
 										i = 6;
 										j = 0;
 									}
-									q6.setState(true);
-									break;
+									//q7.setP(i, j);
+									if(!q7.isConflict(q1) && !q7.isConflict(q2) && !q7.isConflict(q3) && !q7.isConflict(q4) &&
+											!q7.isConflict(q5) && !q7.isConflict(q6)){
+										if (!q7.getState()){
+											q7.setState(true);
+											i = 7;
+											j = 0;
+										}
+										q8.setP(i, j);
+										if(!q8.isConflict(q1) && !q8.isConflict(q2) && !q8.isConflict(q3) && !q8.isConflict(q4) &&
+												!q8.isConflict(q5) && !q8.isConflict(q6) && !q8.isConflict(q7)){
+											q8.setState(true);
+											break;
+										}
+									}
 								}
 							}
 						}
@@ -76,6 +93,8 @@ public class EightQueens {
 		System.out.println("q4 position: " + q4.getP().getHorizontal() + q4.getP().getVertical());
 		System.out.println("q5 position: " + q5.getP().getHorizontal() + q5.getP().getVertical());
 		System.out.println("q6 position: " + q6.getP().getHorizontal() + q6.getP().getVertical());
+		System.out.println("q7 position: " + q7.getP().getHorizontal() + q7.getP().getVertical());
+		System.out.println("q8 position: " + q8.getP().getHorizontal() + q8.getP().getVertical());
 	}
 	
 	
